33229b7964457859c0876fa22c409d4f95cf3964
Paulljc <reborn_cheng@126.com>
2019-07-17 23:47:56 +0800

    finish AC2/3/4/5 in story

--- src/test/java/com/tw/apistackbase/CrowncaseRepositoryTest.java
+++ src/test/java/com/tw/apistackbase/CrowncaseRepositoryTest.java
@@ -7,9 +7,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Date;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -21,11 +23,27 @@ public class CrowncaseRepositoryTest {
     @Test
     public void should_return_case_when_find_by_id() {
         Crowncase crowncase = new Crowncase("murder", new Date());
-
         Crowncase savedCase = crowncaseRepository.saveAndFlush(crowncase);
-
         Assertions.assertNotNull(crowncaseRepository.findById(savedCase.getId()));
     }
 
+    @Test
+    public void should_return_cases_when_find_all_order_by_case_time() {
+        List<Crowncase> happen_time = crowncaseRepository.findAll(new Sort(Sort.Direction.ASC, "caseTime"));
+        System.out.println(happen_time.toString());
+    }
 
+    @Test
+    public void should_equal_size_when_find_by_name() {
+        List<Crowncase> result = crowncaseRepository.findAllByCaseName("murder");
+        Assertions.assertEquals(2, result.size());
+    }
+
+    @Test
+    public void should_return_cases_when_find_by_name() {
+        List<Crowncase> before = crowncaseRepository.findAll();
+        crowncaseRepository.deleteById(3l);
+        List<Crowncase> after = crowncaseRepository.findAll();
+        Assertions.assertSame(1, before.size() - after.size());
+    }
 }
