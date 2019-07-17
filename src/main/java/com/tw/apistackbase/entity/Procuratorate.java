3968983a533632075e50bbf41dcc921649b49b26
Paulljc <reborn_cheng@126.com>
2019-07-18 00:15:51 +0800

    [LJC]:create all entity in story

---
+++ src/main/java/com/tw/apistackbase/entity/Procuratorate.java
@@ -0,0 +1,48 @@
+package com.tw.apistackbase.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Set;
+
+@Entity
+@Table(name = "procuratorate")
+public class Procuratorate {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotNull
+    @Column(unique = true, length = 50)
+    private String name;
+
+    @OneToMany
+    private Set<Prosecutor> prosecutors;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Prosecutor> getProsecutors() {
+        return prosecutors;
+    }
+
+    public void setProsecutors(Set<Prosecutor> prosecutors) {
+        this.prosecutors = prosecutors;
+    }
+
+    public Procuratorate() {
+    }
+}
---
+++ src/main/java/com/tw/apistackbase/entity/Prosecutor.java
@@ -0,0 +1,39 @@
+package com.tw.apistackbase.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "prosecutor")
+public class Prosecutor {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Prosecutor(@NotNull String name) {
+        this.name = name;
+    }
+}
