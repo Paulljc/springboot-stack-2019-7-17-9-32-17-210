fedca241bf8d03ce00a7dead817da9a2a4038314
GUKSON LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUGU2@OOCL.COM>
2019-07-18 11:39:42 +0800

    [Gukson]: finish Procuratorate CRUD test

--- src/test/java/com/tw/apistackbase/ProcuratorateRepositoryTest.java
+++ src/test/java/com/tw/apistackbase/ProcuratorateRepositoryTest.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.List;
+
 @DataJpaTest
 @ExtendWith(SpringExtension.class)
 public class ProcuratorateRepositoryTest {
@@ -24,12 +26,49 @@ public class ProcuratorateRepositoryTest {
     }
 
     @Test
-    public void should_return_the_procuratorate_when_get_procuratorate_by_id() {
+    public void should_return_the_procuratorate_when_find_procuratorate_by_id() {
         Procuratorate procuratorate = new Procuratorate("zha");
-        Procuratorate saveProcuratorate = procuratorateRepository.save(procuratorate);
+        Procuratorate procuratorateInDB = procuratorateRepository.save(procuratorate);
 
-        saveProcuratorate = procuratorateRepository.findById(saveProcuratorate.getId()).get();
+        procuratorateInDB = procuratorateRepository.findById(procuratorateInDB.getId()).get();
 
-        Assertions.assertEquals(procuratorate, saveProcuratorate);
+        Assertions.assertEquals(procuratorate, procuratorateInDB);
+    }
+
+    @Test
+    public void should_return_procuratorate_when_add_procuratorate(){
+        Procuratorate procuratorate = new Procuratorate("zha");
+        Procuratorate procuratorateInDB = procuratorateRepository.save(procuratorate);
+
+        procuratorateInDB = procuratorateRepository.findById(procuratorateInDB.getId()).get();
+
+        Assertions.assertEquals(procuratorate, procuratorateInDB);
+        Assertions.assertEquals(procuratorate.getName(), procuratorateInDB.getName());
+    }
+
+    @Test
+    public void should_return_procuratorate_when_delete_procuratorate(){
+        Procuratorate procuratorate1 = new Procuratorate("when");
+        Procuratorate procuratorate2 = new Procuratorate("what");
+        procuratorateRepository.save(procuratorate1);
+        procuratorateRepository.save(procuratorate2);
+
+        procuratorateRepository.deleteById(procuratorate1.getId());
+
+        List<Procuratorate> allProcuratorate = procuratorateRepository.findAll();
+        Assertions.assertEquals(1, allProcuratorate.size());
+        Assertions.assertFalse(allProcuratorate.contains(procuratorate1));
+    }
+
+    @Test
+    public void should_return_case_detail_when_update_case_detail(){
+        Procuratorate procuratorate = new Procuratorate("zha");
+        procuratorateRepository.save(procuratorate);
+
+        procuratorate.setName("oocl");
+        Procuratorate procuratorateInDB = procuratorateRepository.saveAndFlush(procuratorate);
+
+        Assertions.assertEquals(procuratorate, procuratorateInDB);
+        Assertions.assertEquals(procuratorate.getName(), procuratorateInDB.getName());
     }
 }
